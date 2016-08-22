8ccffc9e305af0e3510f4d3327a67add56cd88e3
Victor Rios <victor_github@outlook.com>
2016-08-22 22:39:00 +0200

    AngularJS + Bootstrap
    Angular starter project

---
+++ source/js/app.js
@@ -0,0 +1,11 @@
+(function () {
+    angular.module('PrototypeApp', []);
+
+    angular.module('PrototypeApp').controller('MainController', function () {
+
+        var controller = this;
+
+        controller.Name = 'PROJECT GORILLA';
+
+    });
+})();
